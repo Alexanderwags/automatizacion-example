0c442d3e67cf67dce0a6c8ac60ac15cc459d62f3
william_ws7🔥 <alexander171296@gmail.com>
2021-11-07 21:37:20 -0400

    feat(add new hook wirh firebase): :art: it was added new hook with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useFirebaseApp } from "reactfire";
 
 function App() {
+  const firebase = useFirebaseApp();
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        {console.log("firebase ", firebase)}
       </header>
     </div>
   );
