ec354a5428bcfe7cdb1cba1f87e5e8ff422c88dd
william_ws7🔥 <alexander171296@gmail.com>
2021-11-07 21:26:59 -0400

    feat: :art: init project

---
+++ src/firebase-config.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDi5xEigspJ6sUp_VVHdMMnTkgq9sE1WEc",
+  authDomain: "example17-a4a33.firebaseapp.com",
+  projectId: "example17-a4a33",
+  storageBucket: "example17-a4a33.appspot.com",
+  messagingSenderId: "349935914644",
+  appId: "1:349935914644:web:e0b0f52375479979582e81",
+};
