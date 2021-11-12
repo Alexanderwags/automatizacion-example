4896d07bf7a324015fc56db5e237105e76ce4832
Alessandra Amicarella <alessamica@gmail.com>
2021-11-11 21:36:59 -0400

    Initial dashboard design

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import OffBulb from "./assets/images/OFFbulb.jpg";
 import OnBulb from "./assets/images/ONbulb.jpg";
 
-import "./App.css";
+import "./App.scss";
 import firebase from "./firebase";
 
 function App() {
@@ -25,14 +25,19 @@ function App() {
 
   return (
     <div className="App">
-      <img src={bulb ? OnBulb : OffBulb} className="bulb" alt="Bulb on/off" />
+      {/* <img src={bulb ? OnBulb : OffBulb} className="bulb" alt="Bulb on/off" />
       <button
         onClick={() => {
           handleClick();
         }}
       >
         {bulb ? "OFF" : "ON"}
-      </button>
+      </button> */}
+      <div className="sidebar">Sidebar</div>
+      <div className="donus">Donus Chart</div>
+      <div className="bulb">Bulb</div>
+      <div className="gauge">Gauge Chart</div>
+      <div className="main">Main?</div>
     </div>
   );
 }
